7dcf23810b107f3f0cfa1dcc4af186f7f6cfd5a1
Tulir293 <tulir293@maunium.net>
2016-03-18 22:58:04 +0200

    Import all packages so that they get compiled (for autocompletion to work)

--- mauircd.go
+++ mauircd.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	_ "github.com/go-sql-driver/mysql"
+	_ "maunium.net/go/mauircd/database"
+	_ "maunium.net/go/mauircd/irc"
+	_ "maunium.net/go/mauircd/plugin"
 )
 
 func main() {
